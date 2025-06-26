a9c301ebe085ec7a7c8a20b5b070b6af20465c67
ts1986 <tomsaunders1@gmail.com>
2025-06-26 10:45:51 +0200

    testing point display feature

---
+++ checking.py
@@ -0,0 +1,16 @@
+import json
+
+
+def loadClubs():
+    with open('clubs.json') as c:
+        listOfClubs = json.load(c)['clubs']
+        return listOfClubs
+
+
+def loadCompetitions():
+    with open('competitions.json') as comps:
+        listOfCompetitions = json.load(comps)['competitions']
+        return listOfCompetitions
+    
+clubs=loadClubs()
+print(clubs)
